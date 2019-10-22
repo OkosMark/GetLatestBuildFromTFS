170ba7c5ffde5c2546b08700c092d3b3b18a9230
Okos <okosm@nala.roche.com>
2019-10-22 16:12:12 +0300

    Add project files.

---
+++ TFSIntegration/Settings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFSIntegration
+{
+    public class Settings
+    {
+        public Uri TfsUrl { get; set; }
+        public string TeamProject { get; set; }
+        public string BuildDefinitionName { get; set; }
+        public string CopyTo { get; set; }
+        public string SourcePathFragment { get; set; }
+        public string SourceFile { get; set; }
+        public int MaxBuildsToKeep { get; set; }
+    }
+}

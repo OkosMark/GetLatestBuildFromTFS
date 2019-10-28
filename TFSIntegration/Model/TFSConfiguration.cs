29d048291a4d93318ce588ff7f234dcc4783ef57
okosm <okosm@RTUMWZB153P0059>
2019-10-28 14:00:14 +0200

    Updated TFSIntegration to work with multiple team projects or build definitions

---
+++ TFSIntegration/Model/TFSConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFSIntegration
+{
+    public class TFSConfiguration
+    {
+        public Uri TfsUrl { get; set; }
+
+        public List<TFSCopySettings> TFSSettings { get; set; }
+    }
+}
--- TFSIntegration/Program.cs
+++ TFSIntegration/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.TeamFoundation.Build.Client;
 using Microsoft.TeamFoundation.Client;
 using Newtonsoft.Json;
@@ -18,58 +19,59 @@ namespace TFSIntegration
         private const string CONFIG_FILE = "TFSIntegrationSettings.json";
 
         static void Main(string[] args)
-        {  
+        {
             Logger.Log.Info("START");
             Logger.Log.Info($"Application runs from this location: {System.Reflection.Assembly.GetExecutingAssembly().Location}");
 
             Logger.Log.Info("Read configuration!");
-            Settings tfsIntegrationSettings = ReadConfiguration(Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), CONFIG_FILE));
+            TFSConfiguration tfsConfiguration = ReadTFSConfiguration(Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), CONFIG_FILE));
 
-            Logger.Log.Info($"Authenticate to server {tfsIntegrationSettings.TfsUrl}");
-            
-
+            Logger.Log.Info($"Authenticate to server {tfsConfiguration.TfsUrl}");
             NetworkCredential credentials = CredentialUtil.GetCredential(
-                tfsIntegrationSettings.TfsUrl.AbsoluteUri.Substring(0, tfsIntegrationSettings.TfsUrl.AbsoluteUri.Length - tfsIntegrationSettings.TfsUrl.AbsolutePath.Length + 1));
-            TfsTeamProjectCollection tfs = new TfsTeamProjectCollection(tfsIntegrationSettings.TfsUrl, credentials);
+                tfsConfiguration.TfsUrl.AbsoluteUri.Substring(0, tfsConfiguration.TfsUrl.AbsoluteUri.Length - tfsConfiguration.TfsUrl.AbsolutePath.Length + 1));
+            TfsTeamProjectCollection tfs = new TfsTeamProjectCollection(tfsConfiguration.TfsUrl, credentials);
             tfs.Authenticate();
-            
+
 
             Logger.Log.Info("Get build server!");
             IBuildServer buildServer = tfs.GetService<IBuildServer>();
             Logger.Log.Info($"Build server version: {buildServer.BuildServerVersion}!");
 
-            Logger.Log.Info($"Get all build definitions from team project {tfsIntegrationSettings.TeamProject}.");
-            IBuildDefinition[] buildDefinitions = buildServer.QueryBuildDefinitions(tfsIntegrationSettings.TeamProject, QueryOptions.Definitions);
+            Parallel.ForEach(tfsConfiguration.TFSSettings, (tfsIntegrationSettings) =>
+                                        {
+                                            Logger.Log.Info($"Get all build definitions from team project {tfsIntegrationSettings.TeamProject}.");
+                                            IBuildDefinition[] buildDefinitions = buildServer.QueryBuildDefinitions(tfsIntegrationSettings.TeamProject, QueryOptions.Definitions);
 
-            if (buildDefinitions == null || !buildDefinitions.Any())
-            {
-                Logger.Log.Info("No build definitions found! Exiting!");
-                return;
-            }
+                                            if (buildDefinitions == null || !buildDefinitions.Any())
+                                            {
+                                                Logger.Log.Info("No build definitions found! Exiting!");
+                                                return;
+                                            }
 
-            Logger.Log.Info($"Found {buildDefinitions.Length} build definitions.");
-            IBuildDefinition buildDefinition =
-                buildDefinitions.Where(build => build.Name == tfsIntegrationSettings.BuildDefinitionName)
-                    .Select(b => b)
-                    .FirstOrDefault();
+                                            Logger.Log.Info($"Found {buildDefinitions.Length} build definitions.");
+                                            IBuildDefinition buildDefinition =
+                                                buildDefinitions.Where(build => build.Name == tfsIntegrationSettings.BuildDefinitionName)
+                                                    .Select(b => b)
+                                                    .FirstOrDefault();
 
-            Logger.Log.Info($"Get build details from {buildDefinition.Name}.");
-            IBuildDetail buildDetail = GetLatestBuildDetails(buildServer, buildDefinition,
-                tfsIntegrationSettings.TeamProject);
+                                            Logger.Log.Info($"Get build details from {buildDefinition.Name}.");
+                                            IBuildDetail buildDetail = GetLatestBuildDetails(buildServer, buildDefinition,
+                                                tfsIntegrationSettings.TeamProject);
 
-            if (buildDetail != null)
-            {
-                DownloadVantageInstaller(
-                    Path.Combine(buildDetail.DropLocation, tfsIntegrationSettings.SourcePathFragment,
-                        tfsIntegrationSettings.SourceFile),
-                    Path.Combine(tfsIntegrationSettings.CopyTo, tfsIntegrationSettings.BuildDefinitionName,
-                        buildDetail.BuildNumber, tfsIntegrationSettings.SourceFile));
-            }
+                                            if (buildDetail != null)
+                                            {
+                                                DownloadVantageInstaller(
+                                                    Path.Combine(buildDetail.DropLocation, tfsIntegrationSettings.SourcePathFragment,
+                                                        tfsIntegrationSettings.SourceFile),
+                                                    Path.Combine(tfsIntegrationSettings.CopyTo, tfsIntegrationSettings.BuildDefinitionName,
+                                                        buildDetail.BuildNumber, tfsIntegrationSettings.SourceFile));
+                                            }
 
-            Logger.Log.Info("Cleanup old builds");
-            CleanUp(Path.Combine(tfsIntegrationSettings.CopyTo, tfsIntegrationSettings.BuildDefinitionName),
-                tfsIntegrationSettings.MaxBuildsToKeep);
-            Logger.Log.Info("FINISHED");
+                                            Logger.Log.Info("Cleanup old builds");
+                                            CleanUp(Path.Combine(tfsIntegrationSettings.CopyTo, tfsIntegrationSettings.BuildDefinitionName),
+                                                tfsIntegrationSettings.MaxBuildsToKeep);
+                                            Logger.Log.Info("FINISHED");
+                                        });          
         }
 
         private static IBuildDetail GetLatestBuildDetails(IBuildServer buildServer, IBuildDefinition def, string teamProjectName)
@@ -102,16 +104,16 @@ namespace TFSIntegration
                 Logger.Log.Info($"Download file from {source} to {destination}");
                 webClient.DownloadFile(source, destination);
             }
-        }
-       
-        private static Settings ReadConfiguration(string configPath)
+        }       
+     
+        private static TFSConfiguration ReadTFSConfiguration(string configPath)
         {
-            Settings config = null;
-         
+            TFSConfiguration config = null;
+
             using (StreamReader file = File.OpenText(configPath))
             {
                 JsonSerializer serializer = new JsonSerializer();
-                config = (Settings)serializer.Deserialize(file, typeof(Settings));
+                config = (TFSConfiguration)serializer.Deserialize(file, typeof(TFSConfiguration));
             }
 
             return config;
